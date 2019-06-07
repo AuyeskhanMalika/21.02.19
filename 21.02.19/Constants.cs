0b55a418ef22ffc4857046fff5891f69c65acc30
auyeskhanm <auyeskhanm@gmail.com>
2019-06-07 19:08:59 +0600

    Добавьте файлы проекта.

---
+++ 21.02.19/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21._02._19
+{
+    public class Constants
+    {
+        public const int FIRST_COURSE = 1;
+        public const int LAST_COURSE = 4;
+    }
+}
